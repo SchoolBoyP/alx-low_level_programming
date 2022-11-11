350c1adac63e9f1eac966fdbcc384e7a60c88948
SchoolBoyP <mukadawambani95@gmail.com>
2022-11-11 12:36:04 -0800

    Tamu

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,31 @@
+nclude "main.h"
+
+#include <stdlib.h>
+
+/**
+
+ * malloc_checked - allocated memory
+
+ * @b: amount of bytes
+
+ * Return: d
+
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+	char *d;
+
+
+
+	d = malloc(b);
+
+	if (d == NULL)
+
+		exit(98);
+
+	return (d);
+
+}
